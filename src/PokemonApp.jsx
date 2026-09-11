bbae489f17ef10c00e08fef37c3a4a691fc3ad52
Rafael Montero <developer@rmontero.me>
2026-09-10 18:56:21 -0600

    Add PokemonSlice Redux store

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,11 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>Pokemon App</h1>
+      <ul>
+        <li>Hola</li>
+        <li>Mundo</li>
+      </ul>
+    </>
+  );
+};
---
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const pokemonSlice = createSlice({
+    name: 'name',
+    initialState: {
+        page: 10,
+        pokemons: [],
+        isLoading: false
+
+    },
+    reducers: {
+        startLoadingPokemons: (state, /* action */ ) => {
+            state.isLoading = true
+        },
+        setPokemons: (state, action) => {
+            console.log(action)
+        }
+    } 
+});
+
+export const { startLoadingPokemons, setPokemons } = pokemonSlice.actions;
